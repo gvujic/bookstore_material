8ca004ba98fa469e36af7ed2fd3875c63701fb72
gvujic <vujic.goran89@gmail.com>
2022-06-13 10:15:50 +0200

    sidenav

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector:'books-app',
     template:`
-        <app-sidenav style="background-color: gray;"></app-sidenav>
+        <app-sidenav></app-sidenav>
     `
 })
 export class BooksComponent{
